4bf38ec54f643d36ed5290952ca2833ea9f2a123
forwardbuilds <forwardbuilds@gmail.com>
2025-09-14 17:30:21 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,7 @@
+
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './global.css'
+import ForwardBuildersLanding from './ForwardBuildersLanding.jsx'
+
+createRoot(document.getElementById('root')).render(<ForwardBuildersLanding />)
